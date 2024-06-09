eb6e2a436afba6e63813f174986f44641815084d
RMM <renatommiguel@gmail.com>
2024-06-10 01:14:18 +0200

    wip

--- portfolio/p3/brython.py
+++
@@ -1,8 +0,0 @@
-from browser import document
-
-def main():
-    a = 5
-    b = 3
-    return a + b
-
-output = main()
--- portfolio/p3/pyodide.py
+++
@@ -1,6 +0,0 @@
-def main():
-    a = 5
-    b = 3
-    return a + b
-
-output = main()
